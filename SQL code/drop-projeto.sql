76082ee574ad1701d6670857f449e61144f820d7
tolojo <73295309+tolojo@users.noreply.github.com>
2021-01-31 18:58:35 +0000

    Delete SQL code directory

--- SQL code/drop-projeto.sql
+++
@@ -1,8 +0,0 @@
-SET FOREIGN_KEY_CHECKS = 0;
-drop table consumo;
-drop table conta;
-drop table plano;
-drop table eletrodomestico;
-drop table tipo_consumo;
-drop table utilizador;
-SET FOREIGN_KEY_CHECKS = 1;
