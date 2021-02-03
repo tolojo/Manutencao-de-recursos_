6d398a189516f7d461b9926b737e9f866f3c5c02
tolojo <73295309+tolojo@users.noreply.github.com>
2021-02-03 21:27:27 +0000

    Delete projeto directory

--- projeto/src/main/java/POO/projeto/projeto/View/ContaView.java
+++
@@ -1,7 +0,0 @@
-package POO.projeto.projeto.View;
-
-public interface ContaView {
-    String getConta_mes();
-    Double getConta_custo();
-    int getConta_id();
-}
--- projeto/src/main/java/POO/projeto/projeto/View/EletrodomesticoView.java
+++
@@ -1,7 +0,0 @@
-package POO.projeto.projeto.View;
-
-public interface EletrodomesticoView {
-    int getEletro_id();
-    String getEletro_nome();
-    double getEletro_potencia();
-}
